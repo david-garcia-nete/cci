cc5cfe3ec6c91a55b3819b4764d93615ae4d422d
David Garcia <david.garcia.nete@gmail.com>
2019-03-13 19:01:07 -0400

    Finish another algorithm.

--- examples/pg70_search.php
+++ examples/pg70_search.php
@@ -5,7 +5,7 @@ define("MAX", 256);
 function compare($arr1, $arr2) 
 { 
     for ($i=0; $i<MAX; $i++) {
-        if ($arr1[i] != $arr2[i]){ 
+        if ($arr1[$i] != $arr2[$i]){ 
             return false; 
         }
         return true; 
@@ -20,12 +20,25 @@ function search($pat, $txt)
   
     // countP[]:  Store count of all characters of pattern 
     // countTW[]: Store count of current window of text 
-    $countP = [];
-    $countTW = []; 
+    $countP = [0]*MAX;
+    $countTW = [0]*MAX; 
     for ($i = 0; $i < $M; $i++) 
     { 
-        ($countP[$pat[$i]])++; 
-        ($countTW[$txt[$i]])++; 
+        $patiOrd = ord($pat[$i]);
+        $txtiOrd = ord($txt[$i]);
+        
+        if(isset($countP[$patiOrd]))
+        {
+            $countP[$patiOrd]++;
+        } else{
+            $countP[$patiOrd] = 0;
+        }
+        if(isset($countTW[$txtiOrd]))
+        {
+            $countTW[$txtiOrd]++;
+        } else{
+            $countP[$patiOrd] = 0;
+        } 
     } 
   
     // Traverse through remaining characters of pattern 
@@ -38,13 +51,36 @@ function search($pat, $txt)
         }
   
         // Add current character to current window 
-        $countTW[$txt[$i]]++; 
+        $txtiOrd = ord($txt[$i]);
+        if(isset($countTW[$txtiOrd]))
+        {
+            $countTW[$txtiOrd]++;
+        } else{
+            $countTW[$txtiOrd] = 0;
+        }
   
+        $txtiMDifOrd = ord($txt[$i-$M]);
         // Remove the first character of previous window 
-        $countTW[$txt[$i-$M]]--; 
+        $countTW[$txtiMDifOrd]--; 
     } 
   
     // Check for the last window in text 
-    if (compare($countP, $countTW)) 
+    if (compare($countP, $countTW)) {
         echo "Found at Index " . $N - $M . "\n";
+    }
 } 
+
+ 
+ 
+ 
+    $txt = "BACDGABCDA"; 
+    $pat = "ABCD"; 
+    search($pat, $txt); 
+ 
+ 
+/*
+Output:
+Found at Index 0
+Found at Index 5
+Found at Index 6
+*/
