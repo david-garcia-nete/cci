56afb89cc9baf8cb997c8e676ede0d9789856b42
david <david@david-p7-1067c>
2019-03-25 00:20:56 -0400

    Update files.

--- examples/pg79_compare_bin_to_hex.php
+++ examples/pg79_compare_bin_to_hex.php
@@ -19,7 +19,7 @@ function convertFromBase($number, $base){
     if ($base < 2 || ($base > 10 && $base != 16)) return -1;
     $value = 0;
     for ($i = strlen($number) - 1; $i >= 0; $i--){
-        $digit = digitToValue(substr($number, $i));
+        $digit = digitToValue(substr($number, $i, 1));
         if ($digit < 0 || $digit >= $base){
             return -1;
         }
