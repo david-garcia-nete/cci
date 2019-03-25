ab9d55867aaea3c0cc2229b42e50bd38b2310f12
david <david@david-p7-1067c>
2019-03-24 22:23:07 -0400

    Update files.

--- examples/pg79_compare_bin_to_hex.php
+++ examples/pg79_compare_bin_to_hex.php
@@ -7,11 +7,26 @@
  */
 
 function compareBinToHex ($binary, $hex){
-    
-    
-    
-    
-    
+    $n1 = convertFromBase($binary, 2);
+    $n2 = convertFromBase($hex, 16);
+    if($n1 < 0 || $n2 < 0){
+        return false;
+    }
+    return $n1 == $n2;    
+}
+
+function convertFromBase($number, $base){
+    if ($base < 2 || ($base > 10 && $base != 16)) return -1;
+    $value = 0;
+    for ($i = strlen($number) - 1; $i >= 0; $i--){
+        $digit = digitToValue(substr($number, $i));
+        if ($digit < 0 || $digit >= $base){
+            return -1;
+        }
+        $exp = strlen($number) - 1 - $i;
+        $value += $digit * pow($base, $exp);
+    }
+    return $value;
 }
 
 compareBinToHex('1111', 'F');
