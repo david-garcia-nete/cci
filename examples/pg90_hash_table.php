2bce57b18e028af6e7cd872985c640d78c19b507
david <david@david-p7-1067c>
2019-03-29 12:13:09 -0400

    Update files.

--- examples/pg90_hash_table.php
+++ examples/pg90_hash_table.php
@@ -61,112 +61,116 @@ class Map {
     public function remove($key) 
     { 
         // Apply hash function to find index for given key 
-        $bucketIndex = getBucketIndex($key); 
+        $bucketIndex = $this->getBucketIndex($key); 
   
         // Get head of chain 
-        HashNode<K, V> head = bucketArray.get(bucketIndex); 
+        $head = $bucketArray->get($bucketIndex); 
   
         // Search for key in its chain 
-        HashNode<K, V> prev = null; 
-        while (head != null) 
+        $prev = null; 
+        while ($head != null) 
         { 
             // If Key found 
-            if (head.key.equals(key)) 
+            if ($head->key == $key) 
                 break; 
   
             // Else keep moving in chain 
-            prev = head; 
-            head = head.next; 
+            $prev = $head; 
+            $head = $head->next; 
         } 
   
         // If key was not there 
-        if (head == null) 
+        if ($head == null) 
             return null; 
   
         // Reduce size 
-        size--; 
+        $size--; 
   
         // Remove key 
-        if (prev != null) 
-            prev.next = head.next; 
+        if ($prev != null) 
+            $prev->next = $head->next; 
         else
-            bucketArray.set(bucketIndex, head.next); 
+            $bucketArray->set($bucketIndex, $head->next); 
   
-        return head.value; 
+        return $head->value; 
     } 
     
+    // Returns value for a key 
+    public function get($key) 
+    { 
+        // Find head of chain for given key 
+        $bucketIndex = $this->getBucketIndex($key); 
+        $head = $bucketArray->get($bucketIndex); 
+  
+        // Search key in chain 
+        while ($head != null) 
+        { 
+            if ($head->key  == $key) 
+                return $head->value; 
+            $head = $head->next; 
+        } 
+  
+        // If key not found 
+        return null; 
+    } 
     
+    // Adds a key value pair to hash 
+    public function add($key, $value) 
+    { 
+        // Find head of chain for given key 
+        $bucketIndex = $this->getBucketIndex($key); 
+        $head = $bucketArray->get($bucketIndex); 
+  
+        // Check if key is already present 
+        while ($head != null) 
+        { 
+            if ($head->key == $key) 
+            { 
+                $head->value = $value; 
+                return; 
+            } 
+            $head = $head->next; 
+        } 
+  
+        // Insert key in chain 
+        $size++; 
+        $head = $bucketArray->get($bucketIndex); 
+        $newNode = new HashNode($key, $value); 
+        $newNode->next = $head; 
+        $bucketArray->set($bucketIndex, $newNode); 
+  
+        // If load factor goes beyond threshold, then 
+        // double hash table size 
+        if ((1.0*$size)/$numBuckets >= 0.7) 
+        { 
+            $temp = $bucketArray; 
+            $bucketArray = []; 
+            $numBuckets = 2 * $numBuckets; 
+            $size = 0; 
+            for ($i = 0; $i < $numBuckets; $i++) 
+                $bucketArray->add(null); 
+  
+            foreach ($temp as $headNode) 
+            { 
+                while ($headNode != null) 
+                { 
+                    $this->add($headNode->key, $headNode->value); 
+                    $headNode = $headNode->next; 
+                } 
+            } 
+        } 
+    } 
     
-    public function set($key, $val) {
-            $i = $orig_i = $this->_get_index($key);
-            $node = new HashTableNode($key, $val);
-            while (true) {
-                    if (!isset($this->_array[$i]) || $key == $this->_array[$i]->key) {
-                            $this->_array[$i] = $node;
-                            break;
-                    }
-                    // increment $i
-                    $i = (++$i % $this->_size);
-                    // loop complete
-                    if ($i == $orig_i) {
-                            // out of space
-                            $this->_double_table_size();
-                            return $this->set($key, $val);
-                    }
-            }
-            return $this;
-    }
-    public function get($key) {
-            $i = $orig_i = $this->_get_index($key);
-            while (true) {
-                    if (!isset($this->_array[$i])) {
-                            return null;
-                    }
-                    $node = $this->_array[$i];
-                    if ($key == $node->key) {
-                            return $node->val;
-                    }
-                    // increment $i
-                    $i = (++$i % $this->_size);
-                    // loop complete
-                    if ($i == $orig_i) {
-                            return null;
-                    }
-            }
-    }
-    private function _get_index($key) {
-            return crc32($key) % $this->_size;
-    }
-    private function _double_table_size() {
-            $old_size = $this->_size;
-            $this->_size *= 2;
-            $data = array(); // new array
-            $collisions = array(); // to be re-added later
-            for ($i=0; $i < $old_size; $i++) {
-                    if (!empty($this->_array[$i])) {
-                            $node = $this->_array[$i];
-                            $j = $this->_get_index($node->key);
-                            // check collisions and record them
-                            if (isset($data[$j]) && $data[$j]->key != $node->key) {
-                                    $collisions[] = $node;
-                            } else {
-                                    $data[$j] = $node;
-                            }
-                    }
-            }
-            $this->_array = $data;
-            // manage collisions
-            foreach ($collisions as $node) {
-                    $this->set($node->key, $node->val);
-            }
-    }
 }
 
 // unit test
-$h = new HashTable();
-$h->set('abc', 'def');
-$h->set('ghi', 'test');
-$h->set('def', 'qbc');
-echo $h->get('abc') . "\n";
-echo $h->get('def') . "\n";
-echo $h->get('ghi') . "\n";
+$map = new Map(); 
+$map.add("this",1 ); 
+$map.add("coder",2 ); 
+$map.add("this",4 ); 
+$map.add("hi",5 ); 
+echo $map->size(); 
+echo $map->remove("this"); 
+echo $map->remove("this"); 
+echo $map->size(); 
+echo $map->isEmpty(); 
