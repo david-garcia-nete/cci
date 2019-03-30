47cd38d9649676bba0825c3c41989463e88a8f73
david <david@david-p7-1067c>
2019-03-30 10:17:37 -0400

    Update files.

--- examples/pg90_array_list.php
+++ examples/pg90_array_list.php
@@ -26,6 +26,20 @@ public function add($anEntry){
     return true;
 }
 
+public function addAtIndex($index, $anEntry){
+    
+    if($index < 0 || $index > $this->size){
+        throw new OutOfBoundsException($index); //Can I pass an int?  Test this. 
+    } 
+    if($this->size == $this->capacity){
+        $this->reallocate();
+    }
+    for($i = $this->size; $i > $index; $i--){
+        $this->theData[$i] = $this->theData[$i-1];
+    }
+    $this->theData[$index] = $anEntry;
+    $this->size++; 
+}
 
     
 }
