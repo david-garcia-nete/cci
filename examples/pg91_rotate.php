d3373c9fd4973b1fc7337f0ec8a6502a74628227
David Garcia <david.garcia.nete@gmail.com>
2019-04-23 23:52:42 -0400

    Save progress.

--- examples/pg91_rotate.php
+++ examples/pg91_rotate.php
@@ -1,55 +1,57 @@
 <?php
 
-function rotate($matrix) {
+class MatrixRotator {
 
-    if (count($matrix) == 0 || count($matrix) != count($matrix[0])) return false; // Not a square
+    public static function rotate(array &$matrix) {
 
-		$n = count($matrix);
+        if (count($matrix) == 0 || count($matrix) != count($matrix[0])) return false; // Not a square
 
-		
+                    $n = count($matrix);
 
-		for (int layer = 0; layer < n / 2; layer++) {
+                    for ($layer = 0; $layer < $n / 2; $layer++) {
 
-			int first = layer;
+                            $first = $layer;
 
-			int last = n - 1 - layer;
+                            $last = $n - 1 - $layer;
 
-			for(int i = first; i < last; i++) {
+                            for($i = $first; $i < $last; $i++) {
 
-				int offset = i - first;
+                                    $offset = $i - $first;
 
-				int top = matrix[first][i]; // save top
-
-
-
-				// left -> top
-
-				matrix[first][i] = matrix[last-offset][first]; 			
+                                    $top = $matrix[$first][$i]; // save top
 
 
 
-				// bottom -> left
+                                    // left -> top
 
-				matrix[last-offset][first] = matrix[last][last - offset]; 
-
-
-
-				// right -> bottom
-
-				matrix[last][last - offset] = matrix[i][last]; 
+                                    $matrix[$first][$i] = $matrix[$last-$offset][$first]; 			
 
 
 
-				// top -> right
+                                    // bottom -> left
 
-				matrix[i][last] = top; // right <- saved top
+                                    $matrix[$last-$offset][$first] = $matrix[$last][$last - $offset]; 
 
-			}
 
-		}
 
-		return true;
-   
+                                    // right -> bottom
+
+                                    $matrix[$last][$last - $offset] = $matrix[$i][$last]; 
+
+
+
+                                    // top -> right
+
+                                    $matrix[$i][$last] = $top; // right <- saved top
+
+                            }
+
+                    }
+
+                    return true;
+
+    }
+
 }
 
 
