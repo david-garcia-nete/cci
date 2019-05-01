b306ae56a400b9d0c962d437d35d083d286e733a
David Garcia <david.garcia.nete@gmail.com>
2019-05-01 18:06:40 -0400

    Save progress.

--- examples/pg94_partition.php
+++ examples/pg94_partition.php
@@ -29,4 +29,82 @@ class LinkedListPartitioner {
         $upperTail->setNext(null);
         return $lowerHead->getNext();
     }
+    
+    public static function partitionCci(Node $node, $x) {
+        $beforeStart = null;
+        $beforeEnd = null;
+        $afterStart = null;
+        $afterEnd = null;
+
+        /* Partition list */
+        while ($node != null) {
+                $next = $node->getNext();
+                $node->setNext(null);
+                if ($node->getData() < x) {
+                        if ($beforeStart == null) {
+                                $beforeStart = $node;
+                                $beforeEnd = $beforeStart;
+                        } else {
+                                $beforeEnd->setNext($node);
+                                $beforeEnd = $node;
+                        }
+                } else {
+                        if ($afterStart == null) {
+                                $afterStart = $node;
+                                $afterEnd = $afterStart;
+                        } else {
+                                $afterEnd->setNext($node);
+                                $afterEnd = $node;
+                        }
+                }	
+                $node = $next;
+        }
+
+        /* Merge before list and after list */
+        if ($beforeStart == null) {
+                return $afterStart;
+        }
+
+        $beforeEnd->setNext($afterStart);
+        return $beforeStart;
+    }
+    
+    public static function partitionCci2(Node $node, $x) {
+        $beforeStart = null;
+        $beforeEnd = null;
+        $afterStart = null;
+        $afterEnd = null;
+
+        /* Partition list */
+        while ($node != null) {
+                $next = $node->getNext();
+                $node->setNext(null);
+                if ($node->getData() < x) {
+                        if ($beforeStart == null) {
+                                $beforeStart = $node;
+                                $beforeEnd = $beforeStart;
+                        } else {
+                                $beforeEnd->setNext($node);
+                                $beforeEnd = $node;
+                        }
+                } else {
+                        if ($afterStart == null) {
+                                $afterStart = $node;
+                                $afterEnd = $afterStart;
+                        } else {
+                                $afterEnd->setNext($node);
+                                $afterEnd = $node;
+                        }
+                }	
+                $node = $next;
+        }
+
+        /* Merge before list and after list */
+        if ($beforeStart == null) {
+                return $afterStart;
+        }
+
+        $beforeEnd->setNext($afterStart);
+        return $beforeStart;
+    }
 }
--- test/pg94_partitionTest.php
+++ test/pg94_partitionTest.php
@@ -55,11 +55,59 @@ class LinkedListPartitionerTest extends \PHPUnit\Framework\TestCase {
         }
         $this->assertEquals(count($this->values), $nodeCount);
     }
+    public function testPartitionCci() {
+        $x = 5;
+        $node = LinkedListPartitioner::partitionCci($this->linkedList, $x);
+        $beforePartition = true;
+        $nodeCount = 0;
+        while ($node !== null) {
+            $data = $node->getData();
+            if ($beforePartition && $data >= $x) {
+                $beforePartition = false;
+            }
+            if ($beforePartition) {
+                $this->assertTrue($data < $x);
+            } else {
+                $this->assertTrue($data >= $x);
+            }
+            $node = $node->getNext();
+            $nodeCount++;
+        }
+        $this->assertEquals(count($this->values), $nodeCount);
+    }
+    public function testPartitionCci2() {
+        $x = 5;
+        $node = LinkedListPartitioner::partitionCci2($this->linkedList, $x);
+        $beforePartition = true;
+        $nodeCount = 0;
+        while ($node !== null) {
+            $data = $node->getData();
+            if ($beforePartition && $data >= $x) {
+                $beforePartition = false;
+            }
+            if ($beforePartition) {
+                $this->assertTrue($data < $x);
+            } else {
+                $this->assertTrue($data >= $x);
+            }
+            $node = $node->getNext();
+            $nodeCount++;
+        }
+        $this->assertEquals(count($this->values), $nodeCount);
+    }
 }
 
 $LinkedListPartitionerTest = new LinkedListPartitionerTest();
 $LinkedListPartitionerTest->setUp();
 $LinkedListPartitionerTest->testPartition();
+$LinkedListPartitionerTest->tearDown();
+
+$LinkedListPartitionerTest->setUp();
+$LinkedListPartitionerTest->testPartitionCci();
+$LinkedListPartitionerTest->tearDown();
+
+$LinkedListPartitionerTest->setUp();
+$LinkedListPartitionerTest->testPartitionCci2();
 $LinkedListPartitionerTest->tearDown();
 
 
